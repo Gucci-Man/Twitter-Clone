8617a693113d0138324d053fab01b10c5e65c091
mrtomno <adel.ngo@yahoo.com>
2023-10-15 12:37:34 -0500

    Update user model tests

--- test_user_model.py
+++ test_user_model.py
@@ -2,15 +2,14 @@
 
 # run these tests like:
 #
-#    python3 -m unittest test_user_model.py
+#    python -m unittest test_user_model.py
 
 
 import os
 from unittest import TestCase
-from flask_bcrypt import Bcrypt
-from models import db, User, Message, Follows
+from sqlalchemy import exc
 
-bcrypt = Bcrypt()
+from models import db, User, Message, Follows
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -28,26 +27,43 @@ from app import app
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
 
-db.drop_all()
 db.create_all()
 
 
 class UserModelTestCase(TestCase):
-    """Test User model."""
+    """Test views for messages."""
 
     def setUp(self):
         """Create test client, add sample data."""
+        db.drop_all()
+        db.create_all()
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        u1 = User.signup("test1", "email1@email.com", "password", None)
+        uid1 = 1111
+        u1.id = uid1
+
+        u2 = User.signup("test2", "email2@email.com", "password", None)
+        uid2 = 2222
+        u2.id = uid2
+
+        db.session.commit()
+
+        u1 = User.query.get(uid1)
+        u2 = User.query.get(uid2)
+
+        self.u1 = u1
+        self.uid1 = uid1
+
+        self.u2 = u2
+        self.uid2 = uid2
 
         self.client = app.test_client()
 
     def tearDown(self):
-        """Clean up fouled transactions"""
-
+        res = super().tearDown()
         db.session.rollback()
+        return res
+
 
     def test_user_model(self):
         """Does basic model work?"""
@@ -65,86 +81,100 @@ class UserModelTestCase(TestCase):
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
 
-    def test_repr_method(self):
-        """Testing repr method"""
-
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
-
-        db.session.add(u)
+    ####
+    #
+    # Following tests
+    #
+    ####
+    def test_user_follows(self):
+        self.u1.following.append(self.u2)
         db.session.commit()
 
-        actual_repr = repr(u)
-        expected_repr = f'<User #{u.id}: testuser, test@test.com>'
-        self.assertEqual(actual_repr, expected_repr)
+        self.assertEqual(len(self.u2.following), 0)
+        self.assertEqual(len(self.u2.followers), 1)
+        self.assertEqual(len(self.u1.followers), 0)
+        self.assertEqual(len(self.u1.following), 1)
+
+        self.assertEqual(self.u2.followers[0].id, self.u1.id)
+        self.assertEqual(self.u1.following[0].id, self.u2.id)
 
     def test_is_following(self):
-        """Test if user1 following user2 """
-
-        user1 = User(
-            email="test1@test.com",
-            username="testuser1",
-            password="HASHED_PASSWORD1"
-        )
-
-        user2 = User(
-            email="test2@test.com",
-            username="testuser2",
-            password="HASHED_PASSWORD2"
-        )
-
-        db.session.add_all([user1, user2])
+        self.u1.following.append(self.u2)
         db.session.commit()
 
-        user1.following.append(user2)
-        db.session.commit()
-
-        # user1 should following user2
-        self.assertIn(user2, user1.following)
+        self.assertTrue(self.u1.is_following(self.u2))
+        self.assertFalse(self.u2.is_following(self.u1))
 
     def test_is_followed_by(self):
-        """Test if user2 is not following user1"""
-
-        user1 = User(
-            email="test1@test.com",
-            username="testuser1",
-            password="HASHED_PASSWORD1"
-        )
-
-        user2 = User(
-            email="test2@test.com",
-            username="testuser2",
-            password="HASHED_PASSWORD2"
-        )
-
-        db.session.add_all([user1, user2])
+        self.u1.following.append(self.u2)
         db.session.commit()
 
-        # user2 should not be following user1
-        self.assertNotIn(user1, user2.following)
+        self.assertTrue(self.u2.is_followed_by(self.u1))
+        self.assertFalse(self.u1.is_followed_by(self.u2))
 
-    def test_authenticate(self):
-        """Test if authentication is succesful"""
-
-        # encrypt password 
-        password="HASHED_PASSWORD"
-        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
-
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password=hashed_pwd
-        )
-
-        db.session.add(u)
+    ####
+    #
+    # Signup Tests
+    #
+    ####
+    def test_valid_signup(self):
+        u_test = User.signup("testtesttest", "testtest@test.com", "password", None)
+        uid = 99999
+        u_test.id = uid
         db.session.commit()
 
-        authenticated_user = User.authenticate("testuser", "HASHED_PASSWORD")
+        u_test = User.query.get(uid)
+        self.assertIsNotNone(u_test)
+        self.assertEqual(u_test.username, "testtesttest")
+        self.assertEqual(u_test.email, "testtest@test.com")
+        self.assertNotEqual(u_test.password, "password")
+        # Bcrypt strings should start with $2b$
+        self.assertTrue(u_test.password.startswith("$2b$"))
 
-        # authentication should pass meaning user will be returned
-        self.assertIs(u, authenticated_user)
+    def test_invalid_username_signup(self):
+        invalid = User.signup(None, "test@test.com", "password", None)
+        uid = 123456789
+        invalid.id = uid
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
 
+    def test_invalid_email_signup(self):
+        invalid = User.signup("testtest", None, "password", None)
+        uid = 123789
+        invalid.id = uid
+        with self.assertRaises(exc.IntegrityError) as context:
+            db.session.commit()
+    
+    def test_invalid_password_signup(self):
+        with self.assertRaises(ValueError) as context:
+            User.signup("testtest", "email@email.com", "", None)
+        
+        with self.assertRaises(ValueError) as context:
+            User.signup("testtest", "email@email.com", None, None)
+    
+    ####
+    #
+    # Authentication Tests
+    #
+    ####
+    def test_valid_authentication(self):
+        u = User.authenticate(self.u1.username, "password")
+        self.assertIsNotNone(u)
+        self.assertEqual(u.id, self.uid1)
+    
+    def test_invalid_username(self):
+        self.assertFalse(User.authenticate("badusername", "password"))
+
+    def test_wrong_password(self):
+        self.assertFalse(User.authenticate(self.u1.username, "badpassword"))
+
+
+
+
+        
+
+
+
+
+        
 
