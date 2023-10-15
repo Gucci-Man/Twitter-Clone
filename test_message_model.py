47b1406a6763ac404c40f5e2ec10ddcea2a5fb26
mrtomno <adel.ngo@yahoo.com>
2023-10-15 12:35:53 -0500

    Update test message models

--- test_message_model.py
+++ test_message_model.py
@@ -2,15 +2,14 @@
 
 # run these tests like:
 #
-#    python3 -m unittest test_message_model.py
+#    python -m unittest test_message_model.py
 
 
 import os
 from unittest import TestCase
-from flask_bcrypt import Bcrypt
-from models import db, User, Message, Follows
+from sqlalchemy import exc
 
-bcrypt = Bcrypt()
+from models import db, User, Message, Follows, Likes
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -28,96 +27,70 @@ from app import app
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
 
-db.drop_all()
 db.create_all()
 
-class MessageModelTestCase(TestCase):
-    """Test Message model"""
+
+class UserModelTestCase(TestCase):
+    """Test views for messages."""
 
     def setUp(self):
         """Create test client, add sample data."""
+        db.drop_all()
+        db.create_all()
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        self.uid = 94566
+        u = User.signup("testing", "testing@test.com", "password", None)
+        u.id = self.uid
+        db.session.commit()
+
+        self.u = User.query.get(self.uid)
 
         self.client = app.test_client()
 
     def tearDown(self):
-        """Clean up fouled transactions"""
-
+        res = super().tearDown()
         db.session.rollback()
+        return res
 
     def test_message_model(self):
-        """Test creating new message"""
-
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
-
-        db.session.add(u)
-        db.session.commit()
-
+        """Does basic model work?"""
+        
         m = Message(
-            text='test message',
-            user_id=u.id
+            text="a warble",
+            user_id=self.uid
         )
 
         db.session.add(m)
         db.session.commit()
 
-        # User should contain message
-        self.assertIn(m, u.messages)
+        # User should have 1 message
+        self.assertEqual(len(self.u.messages), 1)
+        self.assertEqual(self.u.messages[0].text, "a warble")
 
-    def test_show_message(self):
-        """Test showing message contents"""
-
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
+    def test_message_likes(self):
+        m1 = Message(
+            text="a warble",
+            user_id=self.uid
         )
 
-        db.session.add(u)
-        db.session.commit()
-
-        m = Message(
-            text='test message',
-            user_id=u.id
+        m2 = Message(
+            text="a very interesting warble",
+            user_id=self.uid 
         )
 
-        db.session.add(m)
+        u = User.signup("yetanothertest", "t@email.com", "password", None)
+        uid = 888
+        u.id = uid
+        db.session.add_all([m1, m2, u])
         db.session.commit()
 
-        # message content should be the same from user
-        self.assertEqual('test message', u.messages[0].text)
+        u.likes.append(m1)
 
-    def test_delete_message(self):
-        """Test deleting a message"""
-
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
-
-        db.session.add(u)
         db.session.commit()
 
-        m = Message(
-            text='test message',
-            user_id=u.id
-        )
+        l = Likes.query.filter(Likes.user_id == uid).all()
+        self.assertEqual(len(l), 1)
+        self.assertEqual(l[0].message_id, m1.id)
 
-        db.session.add(m)
-        db.session.commit()
 
-        # delete message
-        db.session.delete(m)
-        db.session.commit()
-
-        # user should have no messages
-        self.assertTrue(len(u.messages) == 0)
         
